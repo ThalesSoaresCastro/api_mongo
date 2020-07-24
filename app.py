1a863a35dc635b71d592f3336c3bbc74a61a57da
ThalesCSoares <th4les.castro@gmail.com>
2020-07-24 07:20:31 -0300

    host ok

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MONGODB_SETTINGS']={
     #docker
     #'host':'mongodb://127.0.0.1:27017/videos_api'
     #cloud.mongodb
-    "host":"mongodb+srv://thales1234:thales12345@cluster0.phfwc.mongodb.net/videoapi?retryWrites=true&w=majority"
+    "host":"mongodb+srv://<user>:<passworf>@cluster0.phfwc.mongodb.net/videoapi?retryWrites=true&w=majority"
 }
 
 
